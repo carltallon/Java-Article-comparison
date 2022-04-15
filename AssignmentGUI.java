8ac531ee5433d84179a9a24a5a994919242fc926
carltallon <94623759+carltallon@users.noreply.github.com>
2022-04-15 13:26:31 +0100

    Final changes

--- AssignmentGUI.java
+++ AssignmentGUI.java
@@ -1,81 +1,161 @@
 package assignment1;
 
 import javax.swing.*;
-
-
-
+import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.security.KeyStore.Entry;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.SortedSet;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.concurrent.TimeUnit;
 
 public class AssignmentGUI extends JFrame
 {
 	JTextField stoptext;
+	JTextArea resultsarea;
 	
-    JLabel subheading; 
-
+    JLabel subheading, results; 
     
-    JButton choosefileone;
-    JButton choosefiletwo;
-    
-    JFrame container;
-    
-    JLabel results;
-    JProgressBar resultsbar;
+    JButton choosefileonebutton, choosefiletwobutton,submitbutton;
     
     JFrame filename;
+    JFrame popupframe;
+    
+    JProgressBar resultsbar;
     
     ButtonListener choosefileonelistener;
     ButtonListener2 choosefiletwolistener;
     ButtonListener3 submitbuttonlistener;
     
-    File fileone;
-    File filetwo;
-    ArrayList<String> fileonelist;
-    ArrayList<String> filetwolist;
+    File fileone,filetwo,stopwords;
+
+    ArrayList<String> fileonelist,filetwolist,stopwordlist;
     
-    File stopwords;
-    ArrayList<String> stopwordlist;
-    
-}
-    
+    //class beginning 
     public AssignmentGUI() {
+        
     	//Set title
         setTitle("Text Comparison application");
-        
-        //JFRAME CONTAINER WITH FLOW LAYOUT
-        container = new JFrame();
-        FlowLayout containerlayout = new FlowLayout(FlowLayout.CENTER);
-        container.setLayout(containerlayout);
-         
-        
-        
-        //Creating label input files...
-        subheading = new JLabel();
-        subheading.setText("Input two files to check if they are similar:");
-        
-        container.add(subheading);
+    	
+        //Get the container
+        Container container = getContentPane();
 
+        //Set absolute layout        
+        container.setLayout(null);  
+        container.setBackground(Color.gray);
         
-        //creating a choose file one button
-        choosefileone = new JButton("Choose file one");    
-        container.add(choosefileone); 
-        choosefileonelistener =new ButtonListener();        
-        choosefileone.addActionListener(choosefileonelistener);
+        //Creating heading label..... 
+        final JLabel headinglabel =new JLabel("Input two files to see if they are about the same topic");
+        headinglabel.setSize(350,20);
+        container.add(headinglabel);
+        headinglabel.setLocation(100,10);
         
-        //creating a choose file two button
-        choosefiletwo=new JButton("Choose file two");     
-        container.add(choosefiletwo); 
+
+        //Creating choose file one button
+        choosefileonebutton =new JButton("Choose file one"); 
+        //setting button position and size
+        choosefileonebutton.setSize(125,30);
+        choosefileonebutton.setLocation(100,35); 
+        
+        //set color
+        choosefileonebutton.setBackground(Color.DARK_GRAY);
+        choosefileonebutton.setForeground(Color.white);
+        choosefileonebutton.setFocusPainted(false);
+        container.add(choosefileonebutton); 
+        choosefileonelistener=new ButtonListener();        
+        choosefileonebutton.addActionListener(choosefileonelistener);
+        
+        
+        //Creating choose file two button
+        choosefiletwobutton =new JButton("Choose file two");  
+        //set location and size
+        choosefiletwobutton.setSize(125,30);
+        choosefiletwobutton.setLocation(250,35);
+        
+        //set color 
+        choosefiletwobutton.setBackground(Color.DARK_GRAY);
+        choosefiletwobutton.setForeground(Color.white);
+        choosefiletwobutton.setFocusPainted(false);
+        
+        container.add(choosefiletwobutton); //add to container
+        //add button listener
         choosefiletwolistener = new ButtonListener2();        
-        choosefiletwo.addActionListener(choosefiletwolistener);
+        choosefiletwobutton.addActionListener(choosefiletwolistener);
+
+
+        //Creating label to enter stop words..... 
+        final JLabel stopwordslabel =new JLabel("Enter stop words:");
+        stopwordslabel.setSize(100,20);
+        container.add(stopwordslabel);
+        stopwordslabel.setLocation(100,80);
+        
+        
+        //TEXT for user input for stop words
+        stoptext = new JTextField(10);
+        stoptext.setSize(250,35);
+        container.add(stoptext);
+        stoptext.setLocation(105,100);
+
+
+        //creating button for submission
+        submitbutton = new JButton("Submit");
+        //setting button size and position
+        submitbutton.setSize(140,20);
+        submitbutton.setLocation(150,150);
+        submitbutton.setBackground(Color.DARK_GRAY);
+        submitbutton.setForeground(Color.white);
+        submitbutton.setFocusPainted(false);
+        
+        container.add(submitbutton); //adding to container
+        //adding button listener
+        submitbuttonlistener=new ButtonListener3();        
+        submitbutton.addActionListener(submitbuttonlistener);
+        
+        
+        //Creating label for results..... 
+        JLabel resultslabel =new JLabel("Results:");
+        resultslabel.setSize(100,20);
+        container.add(resultslabel);
+        resultslabel.setLocation(120,200);
+        
+        
+        //creating results bar to display results
+        resultsbar = new JProgressBar();
+		resultsbar.setValue(0);//set value
+		//setting position and size
+		resultsbar.setSize(150,20);
+		resultsbar.setLocation(180,200);
+		resultsbar.setStringPainted(true);
+		container.add(resultsbar);//adding to container
+		
+		
+		//adding results area 
+		resultsarea = new JTextArea("Results appear here...");
+		resultsarea.setSize(250,60);
+		resultsarea.setLocation(120,230);
+		container.add(resultsarea);
+
+    
+        //Set the size of the window and display 
+        setSize(550,350);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
         
         //adding stop words
-        stopwords = new File("C:\\Users\\carlt\\OneDrive - Technological University Dublin\\Documents\\stops.txt");
+        stopwords = new File("stops.txt");
         Scanner scanstops = null;
         stopwordlist = new ArrayList<String>();
 		try {
@@ -88,47 +168,15 @@ public class AssignmentGUI extends JFrame
 		    stopwordlist.add(scanstops.next());
 		}
         
-        //option to add more stop words
-        JLabel stopwordslabel = new JLabel("Add new words to the stops list:");
-        container.add(stopwordslabel);
-        //TEXT for user input
-        stoptext=new JTextField(45);
-        container.add(stoptext);
-       
-		
-        //creating button for submission
-        JButton submitbutton = new JButton("Submit");
-        container.add(submitbutton); 
-        submitbuttonlistener = new ButtonListener3();        
-        submitbutton.addActionListener(submitbuttonlistener);
-        
-        //SHOW RESULTS 
-        results = new JLabel("results:");
-		container.add(results);
-		results.setSize(420,420);
-		
-		resultsbar = new JProgressBar();
-		resultsbar.setValue(0);
-		resultsbar.setBounds(0,0,420,50);
-		resultsbar.setStringPainted(true);
-		container.add(resultsbar);
-		
-		
-        //Set the size of the window and display 
-        container.setSize(550,350);
-        container.setResizable(false);
-        container.setVisible(true);
-        container.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        
-        
     }
     
     //FILE one OPENER 
     private class ButtonListener implements ActionListener {
     	public void actionPerformed(ActionEvent e) {
     		
-    		if (e.getSource() == choosefileone) {
+    		if (e.getSource() == choosefileonebutton) {
     			
+    			//file chooser to open file explorer
     			JFileChooser filechooserone = new JFileChooser();
     			filechooserone.showOpenDialog(null); //select file to open
     			
@@ -146,13 +194,16 @@ public class AssignmentGUI extends JFrame
 					// error handling
 					e2.printStackTrace();
 				}
+				
 				while (scanone.hasNext()){ //input words into list
 				    fileonelist.add(scanone.next());
 				}
-				System.out.println(fileonelist);
-
-    			   			
-    			
+				
+				//remove stop words
+				fileonelist.removeAll( stopwordlist );
+				
+				
+	
     		}
     	}
     }
@@ -162,7 +213,9 @@ public class AssignmentGUI extends JFrame
     {
     	 public void actionPerformed(ActionEvent e)
     	 {
-    		if (e.getSource() == choosefiletwo) {
+    		if (e.getSource() == choosefiletwobutton) {
+    			
+    			//file chooser
     			JFileChooser filechoosertwo = new JFileChooser();
     			filechoosertwo.showOpenDialog(null); //select file to open
     			
@@ -171,20 +224,34 @@ public class AssignmentGUI extends JFrame
 	 			
 	 			JOptionPane.showMessageDialog(filename ,"You have chosen " + filetwo);
 	 			
-	 			
+	 			popupframe = new JFrame();
+				if (fileone == filetwo) {
+	    			JOptionPane.showMessageDialog(popupframe,"The files cannot be the same!");
+	    			System.exit(0);
+	    		}
+				
 	 			//print contents
 	 			Scanner scantwo = null;
 	 			filetwolist = new ArrayList<String>();
-					try {
-						scantwo = new Scanner(filetwo); //scan file variable
-					} catch (FileNotFoundException e1) {
-						// error handling
-						e1.printStackTrace();
-					}
-					while (scantwo.hasNext()){ //input words into list
-					    filetwolist.add(scantwo.next());
-					}
-					System.out.println(filetwolist);
+				try {
+					scantwo = new Scanner(filetwo); //scan file variable
+				} catch (FileNotFoundException e1) {
+					// error handling
+					e1.printStackTrace();
+				}
+				while (scantwo.hasNext()){ //input words into list
+				    filetwolist.add(scantwo.next());
+				}
+				
+				//remove stop words
+				filetwolist.removeAll( stopwordlist );
+				
+				//remove words not in both files
+				filetwolist.retainAll(fileonelist);
+				
+				
+
+					
     		}
     	 }
     }
@@ -192,48 +259,80 @@ public class AssignmentGUI extends JFrame
     private class ButtonListener3 implements ActionListener {
     	
     	public void actionPerformed(ActionEvent e)
-   	 	{
+   	 	{          
+    		//add new stop words
     		String newstops = stoptext.getText();
+			stopwordlist.add(newstops);
+			JOptionPane.showMessageDialog(popupframe,newstops + " added to stop words list");
+			
+			//write stop words to stops.txt file
+			try {
+				FileWriter stopswriter = new FileWriter("stops.txt");
+				stopswriter.write(newstops);
+				stopswriter.close();
+	          	} catch (IOException e3) {
+	          		//TODO Auto-generated catch block
+	          		e3.printStackTrace();
+	          	}
+			
+			//double check stop words after new ones added
+			filetwolist.removeAll( stopwordlist );
+			fileonelist.removeAll( stopwordlist );
             
-    		try {
-    			FileWriter stopswriter = new FileWriter("C:\\Users\\carlt\\OneDrive - Technological University Dublin\\Documents\\stops.txt");
-    			stopswriter.write(newstops);
-    			stopswriter.close();
-    			System.out.println("Successfully wrote to file");
-    		} catch (IOException e3) {
-    			// TODO Auto-generated catch block
-    			e3.printStackTrace();
-    		}
-    		int equal = 0;
-    		
+    	
     		
     		// IF STATEMENT
-            if(equal == 0)
+            if(filetwolist.size() == 0)
             {	
             	resultsbar.setValue(0);
             	resultsbar.setForeground(Color.RED);
-            	JOptionPane.showMessageDialog(resultsframe,"Your files are in no way similar");
+            	resultsarea.setText("");
+            	resultsarea.append("Your files are in no way similar.\nThe files are about different topics.\n The similar words have been written to \na local file.");
+
+            }
+            else if (filetwolist.size() <= 10) {
+            	resultsbar.setValue(25);
+            	resultsbar.setForeground(Color.RED);
+            	resultsarea.setText("");
+            	resultsarea.append("Your files are in no way similar.\nThe files are about different topics.\n The similar words have been written to a \nlocal file.");
             	
             }
             //small similarity
-            else if(equal < 50)
+            else if(filetwolist.size() < 20)
             {
             	resultsbar.setValue(50);
             	resultsbar.setForeground(Color.ORANGE);
-             	JOptionPane.showMessageDialog(resultsframe,"Your files have a small similarity.");
+             	resultsarea.setText("");
+             	resultsarea.append("Your files have a small similarity.\nThe similar words have\n been written to a local file.");
             }    
             // highly similar
-            else if(equal > 70)
+            else if(filetwolist.size() >= 30)
             {
-            	resultsbar.setValue(70);
+            	resultsbar.setValue(100);
             	resultsbar.setForeground(Color.GREEN);
-             	JOptionPane.showMessageDialog(resultsframe, "Your files are very similar.");
+             	resultsarea.setText("");
+             	resultsarea.append("Your files are about the same topic.\nThe similar words have \nbeen written to a local file.");
+             	
             }
             //error
             else
             {
-             	JOptionPane.showMessageDialog(resultsframe,"Error occured.");
+            	resultsarea.setText("");
+            	resultsarea.append("Error occured. Please Try again");
             }
+           
+            //write results
+          try {
+			FileWriter resultswriter = new FileWriter("results.txt");
+			resultswriter.write("Your files had " + filetwolist.size() + " similar words\n");
+			for (String word : filetwolist) {
+				resultswriter.write(word + "\n");
+			}
+			resultswriter.close();
+          	} catch (IOException e3) {
+          		//TODO Auto-generated catch block
+          		e3.printStackTrace();
+          	}
    	 	}
     }
 }
--- Control.java
+++ Control.java
@@ -4,6 +4,7 @@ public class Control {
 	 //Initialize the class
     public static void main(String[] args)
     {
+    	//create new GUI object
         new AssignmentGUI();
     }
 
